23ef50005f0e1939eeac281c54ef665f88fe3732
elyessassi <mjoxen@gmail.com>
2023-10-13 11:28:49 -0700

    Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,11 +3,11 @@
 
 /**
  *main - Entry point
- *Retudrn: Always 0 
+ *return:0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
