100f31bbdd26578e4df639e40dd54e0391dbb3ca
elyessassi <mjoxen@gmail.com>
2023-10-16 03:39:11 -0700

    sign of n

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	{
+		printf("%lu is positive")
+	}
+	if (n==0)
+	{
+		printf("%lu is zero")
+	}
+	if (n<0)
+	{
+		printf("%lu is negative")
+	}
+	return (0);
+}
