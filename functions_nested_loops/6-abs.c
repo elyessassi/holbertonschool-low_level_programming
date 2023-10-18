20d7a3d07f849a88ce23f79e3716b935c82b726c
elyessassi <mjoxen@gmail.com>
2023-10-18 19:09:29 +0100

    main header

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * Return: int*-1 or int
+ * 
+*/
+
+int _abs(int)
+{
+    if (int < 0)
+    {
+        return (int*-1),
+    else
+        return (int)
+    }
+}
