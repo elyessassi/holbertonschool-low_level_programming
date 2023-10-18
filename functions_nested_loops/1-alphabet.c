5a3c7ec57e88aa7a5b719c6c3de4055eebbf8240
elyessassi <mjoxen@gmail.com>
2023-10-18 11:47:55 +0100

    function that prints the alphabet, in lowercase

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,22 +4,27 @@
  * main - entry point
  * Return: always 0
  */
+
+void print_alphabet(void)
+{
+	int x == 97;
+
+	for (; x <= 123 ; x++)
+	{
+		_putchar(x);
+	}
+	return ;
+}
+
 int main()
 {
+	return (0);
+    print_alphabet(x)
+
+
+
+}
+
 	
 
-
-	void print_alphabet(void)
-	{
-		int x == 97;
-
-		for (; x <= 123 ; x++)
-		{
-			_putchar(x);
-		}
-		return ;
-
-	}
-	return (0);
-}
 
