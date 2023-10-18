e14860d46452d7fa2eada5e931c22e91f6b0bcb8
elyessassi <mjoxen@gmail.com>
2023-10-18 09:09:06 +0100

    a program that prints _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
-#include <unistd.h>
 #include <main.h>
+#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - entry point
+ * Return: always 0
  */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+    write("_putchar")
 }
