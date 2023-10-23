12600982ce29484d83b1bc196ebb3adc5da837d2
elyessassi <mjoxen@gmail.com>
2023-10-23 11:59:03 +0100

    a function that prints a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_square - print a square
+ *@size: size of square
+*/
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (x = 0 ; x < n ; x++)
+	{
+		for(y = 0 ; y < n ; y++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n')
+}
