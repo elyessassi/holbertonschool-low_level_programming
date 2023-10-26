8e9edff74f79e32b52a3d50d1a4fc79c4592bc91
elyessassi <mjoxen@gmail.com>
2023-10-26 09:16:39 +0100

    a function that prints a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,18 +10,25 @@
 		int i;
 		int j;
 		int ns;
-
-		for (i = 1 ; i <= size ; i++)
+		
+		if (size <= 0)
 		{
-			ns = size - i;
-			for (j = 1 ; j <= size ; j++)
-			{
-				if (j <= ns)
-					_putchar(32);
-				else
-					_putchar(35);
-			}
 			_putchar('\n');
+		}
+		else
+		{
+			for (i = 1 ; i <= size ; i++)
+			{
+				ns = size - i;
+				for (j = 1 ; j <= size ; j++)
+				{
+					if (j <= ns)
+						_putchar(32);
+					else
+						_putchar(35);
+				}
+				_putchar('\n');
+			}
 		}
 		
 	}
