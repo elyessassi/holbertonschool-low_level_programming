b4d4543e6808de41c4f5c784ad6eaf58caba7eb8
elyessassi <mjoxen@gmail.com>
2023-10-27 13:54:39 +0100

    a function that copies the string pointed to by sr

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcpy - fonction that that copies the string pointed to by src
+ * @dest: destination
+ * @src: initialisation
+ * Return: destination
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+	int len;
+
+	len = strlen(src);
+	for (x = 0 ; x < len ; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
