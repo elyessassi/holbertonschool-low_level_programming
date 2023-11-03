13f72e3460a0fe191fc11072768518d1fe2f9efc
elyessassi <mjoxen@gmail.com>
2023-11-03 12:52:44 +0100

    a program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: num of arguments
+ * @argv: arguments
+ * Return: always 0
+*/
+
+int main(int argc , char *argv[])
+{
+    printf("%d", argv[0]);
+    printf("\n");
+    return (0);
+}
