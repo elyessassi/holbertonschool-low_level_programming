541ce5841ab440bbef3df35705fc0b5be7360c97
elyessassi <mjoxen@gmail.com>
2023-11-09 12:16:14 +0100

    more malloc directory

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,15 @@
 /**
  * malloc_checked - a function that allocates memory using malloc
  * @b: int
-*/
+ * Return: pointer to array
+ */
 
 void *malloc_checked(unsigned int b)
 {
-    int *array;
+	int *array;
 
-    array = malloc(b);
-    if (array == NULL)
-        return (NULL);
-    return (array);
+	array = malloc(b);
+	if (array == NULL)
+		return (98);
+	return (array);
 }
