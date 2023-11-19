2891aec291a8552928214faa119e5afde3f39639
elyessassi <mjoxen@gmail.com>
2023-11-19 08:52:00 +0100

    a function that prints a name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * 
+ * print_name - fonction that prints name
+ * @name: string
+ * @f: fonction pointer that points to fonctions displayng name
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-    if ((name) || f)
-        f(name);
+	if ((name) && f)
+		f(name);
 }
