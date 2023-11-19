6acb6c1d051b81744505e1c8b003b14c22173652
elyessassi <mjoxen@gmail.com>
2023-11-19 10:22:29 +0100

    header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef HEADER
 #define HEADER
+#include <stddef.h>
 
-typedef int size_t;
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
