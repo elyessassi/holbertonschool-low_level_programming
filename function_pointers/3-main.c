bdec8fcd38d2132766ed46d991857a685dbedda1
elyessassi <mjoxen@gmail.com>
2023-11-21 13:49:06 +0100

    main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,7 +3,10 @@
 #include <stddef.h>
 
 /**
- * 
+ * main - entry point
+ * @argc: numbre of arguments
+ * @argv: elements
+ * Return: always 0
 */
 
 int main(int argc, char *argv[])
@@ -17,15 +20,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(argv[2]) == NULL)
+	if (get_op_func(argv[2]))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%'))
 	{
+		if (*argv[3] == 0)
+		{
 		printf("Error\n");
 		exit(100);
+		}
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
