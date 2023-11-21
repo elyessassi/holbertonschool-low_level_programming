f9a20b6283b039cd43271b748e7a34cb2db4f1de
elyessassi <mjoxen@gmail.com>
2023-11-21 13:40:38 +0100

    operaton function

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - fonction that sums a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: a + b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - fonction that gives the difference between a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: a - b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - fonction that gives the result of a multipied by b
+ * @a: int 1
+ * @b: int 2
+ * Return: a * b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - fonction that gives the division of a by b
+ * @a: int 1
+ * @b: int 2
+ * Return: a / b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - fonction that returns the reminder of the divitsion
+ * @a: int 1
+ * @b: int 2
+ * Return: a % b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
