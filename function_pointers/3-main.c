f1061d1d22602733dc5f6c3704609c1ce796cc31
elyessassi <mjoxen@gmail.com>
2023-11-21 13:41:05 +0100

    main

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * 
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int (*ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	ptr = get_op_func(argv[2]);
+	printf("%d", ptr(num1, num2));
+	printf("\n");
+	return (0);
+}
