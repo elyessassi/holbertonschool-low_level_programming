9b57913c069d16c857d63d47835a9e6b5e12a6e3
elyessassi <mjoxen@gmail.com>
2023-11-22 09:18:32 +0100

    header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,22 @@
 #ifndef VARFONC
 #define VARFONC
+#include <stdarg.h>
 
+/**
+ * struct type - data structure
+ * @character: character
+ * @fptr: function pointer that holds print functions
+*/
+
+typedef struct type
+{
+	char character;
+	void (*fptr)(va_list);
+} type_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
