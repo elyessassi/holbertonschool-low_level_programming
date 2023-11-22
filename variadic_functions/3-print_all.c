589cf732010fe70747bef17e2ba7d2584b563254
elyessassi <mjoxen@gmail.com>
2023-11-22 09:18:07 +0100

    a function that prints anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,88 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+/**
+ * pc - fonc that prints a char
+ * @a: allocated places
+*/
+
+void pc(va_list a)
+{
+	printf("%c", va_arg(a, int));
+}
+
+/**
+ * pi - fonc that prints an int
+ * @a: allocated places
+*/
+
+void pi(va_list a)
+{
+	printf("%d", va_arg(a, int));
+}
+
+/**
+ * pf - fonc that prints a float
+ * @a: allocated places
+*/
+
+void pf(va_list a)
+{
+	printf("%f", va_arg(a, double));
+}
+
+/**
+ * ps - fonc that prints a string
+ * @a: allocated places
+*/
+
+void ps(va_list a)
+{
+	printf("%s", va_arg(a, char *));
+}
+
+
+/**
+ * print_all - main function
+ * @format: string that has format characters
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list numarg;
+	type_t typelist[] = {{'c', pc}, {'i', pi}, {'f', pf}, {'s', ps}};
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int len = strlen(format);
+	void (*ffptr)(va_list);
+
+	va_start(numarg, format);
+	while (i < len)
+	{
+		while (j < 4)
+		{
+			if (format[i] == typelist[j].character)
+			{
+				ffptr = typelist[j].fptr;
+				ffptr(numarg);
+				if (k < len - 2)
+				{
+					printf(", ");
+					k++;
+				}
+				break;
+			}
+			else
+				j++;
+		}
+		j = 0;
+		i++;
+	}
+	printf("\n");
+	va_end(numarg);
+}
