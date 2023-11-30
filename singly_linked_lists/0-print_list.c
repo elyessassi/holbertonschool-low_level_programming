5af7f1c850fa3fbe76cc730b885156cbf1c99e14
elyessassi <mjoxen@gmail.com>
2023-11-30 10:31:55 +0100

    print content

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * 
+*/
+
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	for (i = 0 ; h != NULL ; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s", h->len, h->str);
+			printf("\n");
+			h = h->next;
+		}
+	}
+	return (i);
+}
