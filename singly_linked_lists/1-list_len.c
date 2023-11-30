7e0dfb8ec9a2215fcc2cf85c194373d0d2a997ae
elyessassi <mjoxen@gmail.com>
2023-11-30 10:39:55 +0100

    numbre of nodes

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * 
+*/
+
+size_t list_len(const list_t *h)
+{
+    int i;
+    
+    for (i = 0 ; h != NULL ; i++)
+    {
+        h = h->next;
+    }
+    return (i);
+}
