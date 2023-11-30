802b7f707e937a21f5fe6eea987096270cc3f89f
elyessassi <mjoxen@gmail.com>
2023-11-30 10:42:21 +0100

    numbre of nodes

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,16 +1,18 @@
 #include "lists.h"
 
 /**
- * 
+ * list_len - func that countes the numbre of nodes
+ * @h: node
+ * Return: numbre of nodes
 */
 
 size_t list_len(const list_t *h)
 {
-    int i;
-    
-    for (i = 0 ; h != NULL ; i++)
-    {
-        h = h->next;
-    }
-    return (i);
+	int i;
+
+	for (i = 0 ; h != NULL ; i++)
+	{
+		h = h->next;
+	}
+	return (i);
 }
