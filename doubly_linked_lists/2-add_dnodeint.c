931b9842dd5275f14c10feb33a3c5b1d8e5f2750
elyessassi <mjoxen@gmail.com>
2023-12-03 11:11:10 +0100

    add a node

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - func that adds a new node to the beginning
+ * @n: data to add to the new node
+ * @head: head of list
+ * Return: NULL or the node
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
+
+	if (newnode == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->prev = NULL;
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
