3b1c9e015f9a1f324f26f42ccaaf7bb85dcdcd0e
elyessassi <mjoxen@gmail.com>
2023-12-03 15:38:49 +0100

    sum of elements in list

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -3,21 +3,21 @@
 /**
  * sum_dlistint - sum of elements in list
  * @head: head of list
- * Return 0 or the sum
+ * Return: 0 or the sum
 */
 
 int sum_dlistint(dlistint_t *head)
 {
-    int i = 0;
+	int i = 0;
 
-    if (head == NULL)
-    {
-        return (0);
-    }
-    while (head != NULL)
-    {
-        i = i + head->n;
-        head = head->next;
-    }
-    return (i);
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
+	{
+		i = i + head->n;
+		head = head->next;
+	}
+	return (i);
 }
