273f3f067943a11a1d710ce7e0e886548bcf14e7
elyessassi <mjoxen@gmail.com>
2023-12-05 16:45:25 +0100

    just testing

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * 
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    int n;
+    char buf[1000];
+
+    if (!filename)
+    {
+        return (0);
+    }
+    fd = open(filename,O_RDONLY);
+    if (fd == -1)
+    {
+        return (0);
+    }
+    n = read(fd, buf, letters);
+    n = write(STDOUT_FILENO, buf, n);
+    close(fd);
+    return (n);
+}
