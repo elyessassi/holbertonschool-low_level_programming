7b19b2bb2a1781565f06878f09fccb518df8f1b4
elyessassi <mjoxen@gmail.com>
2023-12-05 16:55:53 +0100

    read a file and write its content

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,26 +3,29 @@
 #include <fcntl.h>
 
 /**
- * 
+ * read_textfile: func that reads a file and writes it to the standard output
+ * @filename: file name
+ * @letters: numbers of letters to display
+ * Return: 0 if failed or the number of letters displayed
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    int n;
-    char buf[10000];
+	int fd;
+	int n;
+	char buf[10000];
 
-    if (!filename)
-    {
-        return (0);
-    }
-    fd = open(filename,O_RDONLY);
-    if (fd == -1)
-    {
-        return (0);
-    }
-    n = read(fd, buf, letters);
-    n = write(STDOUT_FILENO, buf, n);
-    close(fd);
-    return (n);
+	if (!filename)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	n = read(fd, buf, letters);
+	n = write(STDOUT_FILENO, buf, n);
+	close(fd);
+	return (n);
 }
