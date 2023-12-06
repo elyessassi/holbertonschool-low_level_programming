a899c3b5cfaac970ef4510c35ccd7c1531e0814e
elyessassi <mjoxen@gmail.com>
2023-12-06 08:38:55 +0100

    create a file and add text

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		fd = open(filename, O_CREAT, 0600);
 		return (1);
 	}
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY);
 	write(fd, text_content, strlen(text_content));
 	return (1);
 }
