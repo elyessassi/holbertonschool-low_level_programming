7dd00956555709eb5d9da773d465c3ef94e82331
elyessassi <mjoxen@gmail.com>
2023-12-06 13:34:09 +0100

    program test

---
+++ file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * 
+*/
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    int x;
+    int y;
+    int fd1;
+    int z;
+    char buffer[10000];
+
+    if (argc != 3)
+    {
+        dprintf(3, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        dprintf(3, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    x = read(fd, buffer, 10000);
+    close(fd);
+    fd1 = open(argv[2], O_WRONLY | O_TRUNC);
+    y = write(fd1, buffer, x);
+    if (y == -1)
+    {
+        dprintf(3, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
+    }
+    z = close(fd1);
+    if (z == -1)
+    {
+        dprintf(3, "Error: Can't close fd %d", fd1);
+        exit(100);
+    }
+    return (1);
+
+
+
+    
+}
