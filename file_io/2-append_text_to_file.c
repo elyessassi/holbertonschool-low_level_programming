a573054574c6d67861bc8471d577eb3368c6bb9a
elyessassi <mjoxen@gmail.com>
2023-12-06 11:02:47 +0100

    append text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,6 +25,10 @@ int append_text_to_file(const char *filename, char *text_content)
         }
     }
     fd = open(filename, O_APPEND|O_WRONLY);
+    if (fd == -1)
+    {
+        return (-1);
+    }
     write(fd, text_content, strlen(text_content));
     close(fd);
     return (1);
