541771f6963dce6188169186422fdcf4dcc3e965
elyessassi <mjoxen@gmail.com>
2023-12-06 17:54:35 +0100

    program test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,47 +10,47 @@
 
 int main(int argc, char *argv[])
 {
-    int fd;
-    int x;
-    int y;
-    int fd1;
-    int z;
-    char buffer[10000];
+	int fd;
+	int x;
+	int y;
+	int fd1;
+	int z;
+	char buffer[10000];
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    x = read(fd, buffer, 10000);
-    if (x == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    close(fd);
-    fd1 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-    y = write(fd, buffer, x);
-    if (y == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
-    z = close(fd1);
-    if (z == -1)
-    {
-        dprintf(3, "Error: Can't close fd %d", fd1);
-        exit(100);
-    }
-    return (0);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	x = read(fd, buffer, 10000);
+	if (x == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	close(fd);
+	fd1 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	y = write(fd, buffer, x);
+	if (y == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	z = close(fd1);
+	if (z == -1)
+	{
+		dprintf(3, "Error: Can't close fd %d", fd1);
+		exit(100);
+	}
+	return (0);
 
 
 
-    
+	
 }
