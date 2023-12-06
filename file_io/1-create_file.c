9a7156b9f09f9e519ee082644fc3ec8dbfb34cc7
elyessassi <mjoxen@gmail.com>
2023-12-06 08:36:22 +0100

    create a file and add text

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,6 +3,9 @@
 
 /**
  * create_file - func that creates a file
+ * @filename: name of file to be created
+ * @text_content: text to put in the file
+ * Return: 1 on success -1 on failure
 */
 
 int create_file(const char *filename, char *text_content)
@@ -12,6 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 	{
 		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		return (1);
 	}
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	write(fd, text_content, strlen(text_content));
