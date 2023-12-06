2565590617bfdc9f7df9a97ee4992adc0d4be9eb
elyessassi <mjoxen@gmail.com>
2023-12-06 10:41:30 +0100

    append text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,9 +8,21 @@ int append_text_to_file(const char *filename, char *text_content)
 {
     int fd;
 
-    if (!filename || !text_content)
+    if (!filename)
     {
         return (-1);
+    }
+    if (!text_content)
+    {
+        fd = open(filename, O_APPEND);
+        if (fd == -1)
+        {
+            return (-1);
+        }
+        else
+        {
+            return(1);
+        }
     }
     fd = open(filename, O_APPEND);
     write(fd, text_content, strlen(text_content));
