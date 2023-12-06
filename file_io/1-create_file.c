497684a696f5e13d4df4bd9ae10919806711db9a
elyessassi <mjoxen@gmail.com>
2023-12-06 12:32:26 +0100

    create a file and add text

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,10 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	if (!filename)
-	{
-		return (-1);
-	}
+	
 	if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT, 0600);
