068377d81a51c44405bf6eb6a95fce5e74a45714
elyessassi <mjoxen@gmail.com>
2023-12-07 23:22:05 +0100

    program test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[])
 	int x;
 	int y;
 	int fd1;
-	int z;
+	int close1;
+	int close2;
 	char buffer[10000];
 
 	if (argc != 3)
@@ -29,16 +30,31 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	x = read(fd, buffer, 10000);
-	close(fd);
+	if (x == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	close1 = close(fd);
+	if (close1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fd1 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (y == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	y = write(fd, buffer, x);
 	if (y == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	z = close(fd1);
-	if (z == -1)
+	close2 = close(fd1);
+	if (close2 == -1)
 	{
 		dprintf(3, "Error: Can't close fd %d", fd1);
 		exit(100);
