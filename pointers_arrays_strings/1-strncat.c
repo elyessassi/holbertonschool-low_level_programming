b09e0f1bfb1f56ccc93b442df26b24461b1bcee6
elyessassi <mjoxen@gmail.com>
2024-01-01 10:56:36 +0100

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - copies n letters of src to dest
+ * @dest: destination
+ * @src: source
+ * @n: numbre of letters
+ * Return: string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j != n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
