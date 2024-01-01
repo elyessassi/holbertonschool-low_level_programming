8aef6867a5c7cbbda8c122c2afb286a35e9a2aa7
elyessassi <mjoxen@gmail.com>
2024-01-01 11:13:32 +0100

    strcpy copy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a number of bytes of a string to an other
+ * @dest: destination
+ * @src: source
+ * @n: number of letters
+ * Return: dest string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i == n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
