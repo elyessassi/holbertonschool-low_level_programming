0f3dc834ef51889c3bf91123ffec2e2088418921
elyessassi <mjoxen@gmail.com>
2024-01-01 10:48:27 +0100

    fix

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
- * 
+ * _strcat - copies a string to an other string
+ * @dest: destination
+ * @src: source
+ * Return: destination
 */
 
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0 ; src[j] != '\0' ; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
