35ebee6b3d0609913d959963448aa763408d5ce7
elyessassi <mjoxen@gmail.com>
2024-04-05 23:07:33 +0000

    linear search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+*@array: array to search in
+*@size: size of array
+*@value: value to search for
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t current_idx;
+
+	if (array == NULL)
+		return (-1);
+	for (current_idx = 0 ; current_idx < size ; current_idx++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", current_idx, array[current_idx]);
+		if ((array[current_idx]) == value)
+			return (array[current_idx]);
+	}
+	return (-1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SA
+#define SA
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
