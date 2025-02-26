6df8f0c2fd9cc7568db4c12ca0d8c658931419a2
elyessassi <mjoxen@gmail.com>
2025-02-26 21:19:33 +0100

    adding _strcmp prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,3 +16,4 @@ void _puts_recursion(char *s);
 int factorial(int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
