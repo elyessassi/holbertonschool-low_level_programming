3a078d676f093f032821ee57294ba846e11c0fb6
elyessassi <mjoxen@gmail.com>
2025-03-04 23:40:23 +0100

    applying betty style

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -6,16 +6,16 @@
  * @b: value to fill with
  * @n: number of bytes to fill
  * Return: pointer to the memory area
-*/
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i;
-    int x = n;
+	int i;
+	int x = n;
 
-    for (i = 0 ; i < x ; i++)
-    {
-        s[i] = b;
-    }
-   return (s);
+	for (i = 0; i < x; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
