3f54340afc3f60fdc4ba2563ecf6021fcdbbcff7
elyessassi <mjoxen@gmail.com>
2025-03-05 10:28:36 +0100

    adding _strspn

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,3 +24,5 @@ int is_separator(char x);
 char *leet(char *);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+int exists(char c, char *s);
