bb9a7e92735f1cbfdc0d9fedd5b6c0a02987a740
elyessassi <mjoxen@gmail.com>
2025-03-06 22:52:59 +0100

    handling cases where nodes are inserted in the start and the end of the d linked list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,14 +10,11 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode = malloc(sizeof(dlistint_t));
-	dlistint_t *temp = *h;
+	dlistint_t *newnode = malloc(sizeof(dlistint_t)), *temp = *h;
 	unsigned int i = 0;
 
 	if (newnode == NULL)
-	{
 		return (NULL);
-	}
 	newnode->n = n;
 	if (*h == NULL)
 	{
@@ -26,18 +23,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		newnode->prev = NULL;
 		return (newnode);
 	}
-	while (temp->next != NULL)
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	while (temp != NULL)
 	{
 		if (i == (idx - 1))
 		{
-			newnode->next = temp->next;
-			temp->next = newnode;
-			newnode->prev = temp;
-			if (newnode->next != NULL)
+			if (temp->next == NULL)
 			{
-				newnode->next->prev = newnode;
+				add_dnodeint_end(h, n);
+				return (*h);
 			}
-			return (newnode);
+			else
+			{
+				newnode->next = temp->next;
+				temp->next = newnode;
+				newnode->prev = temp;
+				if (newnode->next != NULL)
+					newnode->next->prev = newnode;
+				return (newnode);
+			}
 		}
 		temp = temp->next;
 		i++;
