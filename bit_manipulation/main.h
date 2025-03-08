4c7f78374dd8844fcbfa4774e97f6895e486ab3f
elyessassi <mjoxen@gmail.com>
2025-03-08 16:46:58 +0100

    adding prototypes for get_bit and binary_length

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -6,6 +6,8 @@ int power(int x, int y);
 int strlength(const char *s);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int binary_length(unsigned long int n);
 
 
 #endif
